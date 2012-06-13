5ec77c26c80533465c80b3f1cbac06489b72518b
Ric Allinson <capecodehq@yahoo.co.uk>
2012-06-12 19:01:57 -0700

    Got it working!

--- example/second/index.js
+++ example/second/index.js
@@ -1,193 +1,47 @@
 
-var arena,
-    Pixel = require('../../lib/pixel'),
-    pixels = [],
-    counter = 0;
+var Pixel = require('../../lib/pixel'),
+    engine = require('../../lib/engine'),
+    pixels = [];
 
-function makeArena(x, y) {
-    var arena = [],
-        maxX,
-        maxY;
-
-    for (maxY = 0; maxY < y; maxY = maxY + 1) {
-        arena[maxY] = [];
-        for (maxX = 0; maxX < x; maxX = maxX + 1) {
-            arena[maxY][maxX] = null;
-        }
-    }
-
-    return arena;
-}
-
-function addPixelsToArena(pixels, arena) {
-    var y = arena.length - 1,
-        x = arena[0].length - 1,
-        randY,
-        randX,
-        pixel,
-        store = {};
-
-    for (pixel in pixels) {
-        
-        do {
-            randY = Math.floor(Math.random()*y);
-            randX = Math.floor(Math.random()*x);
-            
-            if (!store[randY+':'+randX]) {
-                store[randY+':'+randX] = true;
-                pixels[pixel].space.x = randX;
-                pixels[pixel].space.y = randY;
-            }
-            
-        } while (!store[randY+':'+randX]);
-    }
-}
-
-function findClosestPixel(pixels, pixel) {
-
-    var i, match,
-        current,
-        currentDist,
-        toX,
-        toY,
-        fromX = pixel.space.x,
-        fromY = pixel.space.y;
-
-    for (i in pixels) {
-        current = pixels[i];
-
-        if (current.available()) {
-            toX = current.space.x;
-            toY = current.space.y;
-
-            var dx = fromX - toX;
-            var dy = fromY - toY;
-            var dist = Math.sqrt(dx*dx + dy*dy);
-
-            if (!currentDist || dist < currentDist) {
-                currentDist = dist;
-                match = current;
-            }
-        }
-    }
-
-    return match;
-}
-
-function movePixel(from, to) {
-
-    var fromX = from.space.x,
-        fromY = from.space.y,
-        toX = to.space.x,
-        toY = to.space.y;
-
-    /*
-     * First see if they are touching
-     */
-
-    if ((toX === fromX || toX === fromX + 1 || toX === fromX - 1) && (toY === fromY || toY === fromY + 1 || toY === fromY - 1)) {
-        return true;
-    }
-
-    /*
-     * If they are not then move them
-     */
-
-    if (toX < fromX) {
-        fromX = fromX - 1;
-    } else {
-        fromX = fromX + 1;
-    }
-
-    if (toY < fromY) {
-        fromY = fromY - 1;
-    } else {
-        fromY = fromY + 1;
-    }
-
-    if (fromX < 0) {
-        fromX = 0;
-    }
-
-    from.space.x = fromX;
-    from.space.y = fromY;
-
-    return false;
-}
-
-function cycle(pixels) {
-    var i, pixel,
-        match;
-    
-    for (i in pixels) {
-        pixel = pixels[i];
-        if (pixel.want > 0) {
-            match = findClosestPixel(pixels, pixel);
-            if (match) {
-                if (movePixel(match, pixel)) {
-                    pixel.attach(match);
-                }
-            }
-        }
-    }
-}
-
-function paint(arena, pixels, current) {
-    var frame = '',
-        pixel,
-        x, y,
-        pX, pY;
-
-    /*
-     * For each pixel if it within the arena draw it.
-     */
-    for (pixel in pixels) {
-        pX = pixels[pixel].space.x;
-        pY = pixels[pixel].space.y;
-
-        if (arena[pY] && arena[pY][pX] === null) {
-            arena[pY][pX] = pixels[pixel].want.toString();
-        }
-    }
-
-    /*
-     * Now paint the frame
-     */
-    for (y = 0; y < arena.length; y = y + 1) {
-        frame = frame = frame + '\n';
-        for (x = 0; x < arena[y].length; x = x + 1) {
-            frame = frame + (arena[y][x] ? arena[y][x] : ' ') + ' ';
-        }
-    }
-
-    console.log('\033[2J');
-    console.log('Frame: ' + current);
-    console.log(frame);
-}
+var map = [
+    [0,1,0,0,1],
+    [1,1,1,1,1],
+    [0,1,0,0,1],
+    [0,1,0,0,1],
+    [0,1,0,1,1],
+];
 
 /*
  * Ganerate Pixels
  */
-for (var i = 0; i < 20; i++) {
+for (var i = 0; i < 50; i++) {
     pixels.push(Pixel.create());
 }
 
-pixels[0].program([[0,1,0],[1,1,1],[0,1,0],[0,1,0],[1,1,1],[0,1,0]], {x: 1, y: 0});
+/*
+ * Plot pixels
+ */
+engine.plotPixelsToXY(pixels, 30, 30);
 
 /*
- * Make the arena
+ * Add seed pixel
  */
-arena = makeArena(30, 30);
+pixels[0].program(map, {x: 1, y: 0});
+pixels[1].program(map, {x: 1, y: 0});
 
-addPixelsToArena(pixels, arena);
-
-paint(makeArena(30, 30), pixels, 0);
-
+var counter = 0;
 var id = setInterval(function () {
     if (counter >= 400) {
         clearInterval(id);
     }
-    cycle(pixels);
-    paint(makeArena(30, 30), pixels, counter);
+    
+    engine.cycle(pixels);
+    
+    frame = engine.paint(pixels, 30, 30);
+
+    console.log('\033[2J');
+    console.log('Frame: ' + (counter));
+    console.log(frame);
+
     counter = counter + 1;
-}, 200);
+}, 20);
